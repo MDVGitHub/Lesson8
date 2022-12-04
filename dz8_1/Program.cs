1cc1a6ee0cc4858a5f2b5c715b72f5572c9adc21
Dmitriy Makarov <d.makarov.mail@gmail.com>
2022-12-04 23:32:03 +0300

    dz8_1

--- dz8_1/Program.cs
+++ dz8_1/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+
+int[,] generate2DArray(int lengthRow, int lengthCol, int deviation)
+{
+    int[,] array = new int[lengthRow, lengthCol];
+    for (int i = 0; i < lengthRow; i++)
+    {
+        for (int j = 0; j < lengthCol; j++)
+        {
+            array[i, j] = new Random().Next(-deviation, deviation + 1);
+        }
+    }
+    return array;
+}
+int[,] generateSortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int buf = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = buf;
+                }
+            }
+
+        }
+    }
+    return array;
+}
+
+void printArray(int[,] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] array = generate2DArray(4, 4, 10);
+Console.WriteLine("Задан случайный массив");
+printArray(array);
+Console.WriteLine();
+Console.WriteLine("Массив упорядочен построчно по убыванию");
+int[,] sortarray = generateSortArray(array);
+printArray(sortarray);
