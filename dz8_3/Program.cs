58cbc7989e598a4ad543fe900694adb319985a56
Dmitriy Makarov <d.makarov.mail@gmail.com>
2022-12-05 03:03:56 +0300

    dz8_3

---
+++ dz8_3/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)*/
+int[,,] Get3DArray(int a, int b, int c)
+{
+    int[,,] array = new int[a, b, c];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = random.Next(10, 99 + 2);
+            }
+        }
+    }
+    return array;
+}
+void Print3DArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($" {array[i, j, k]} ({i}, {j}, {k})" + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+int[,,] array = Get3DArray(2, 2, 2);
+Print3DArray(array);
